ed2c10be58f53fe1f6a2681462bf26d0c988db2f
Andrian Nobella <57834926+andriannobella@users.noreply.github.com>
2020-08-12 21:26:08 +0700

    Polymorphism

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from geometri.persegipanjang import PersegiPanjang
 from geometri.segitiga import Segitiga
 
 
-print('Menggunakan OOP')
+'''
+Encapsulation
+'''
 p1 = PersegiPanjang(10, 3)
 print(p1.info())
 print(p1.hitung_luas())
@@ -14,7 +16,21 @@ s1 = Segitiga(4, 2)
 print(s1.info())
 print(s1.hitung_luas())
 
+'''
+inheritance
+'''
 print('\nMencoba membuat objek dari kelas BangunRuang')
 b1 = BangunRuang()
 print(b1.info())
 print(b1.hitung_luas())
+
+
+'''
+Polymorphism
+'''
+daftar_bangun_ruang = []
+daftar_bangun_ruang.append(p1)
+daftar_bangun_ruang.append(s1)
+
+for bangun_ruang in daftar_bangun_ruang:
+    print(bangun_ruang.info())
